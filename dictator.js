a2e15b5449c27354d209760436912daa275b893e
Charles Szilagyi <charles@charlesagile.com>
2017-04-06 21:12:27 +0100

    ✨ Add support for lists

--- dictator.js
+++ dictator.js
@@ -1,9 +1,52 @@
 import {createAction, handleActions} from 'redux-actions'
 
+const isListProp = (propName) => propName.endsWith('List')
+
+const propNameToAdd = (propName) => `${propName}Add`
+const propNameToRemove = (propName) => `${propName}Remove`
+
+const setterAction = (propName) => ({
+    [propName]: (state, {payload}) => ({...state, [propName]: payload})
+})
+
+const listAddAction = (propName) => ({
+    [propNameToAdd(propName)]: (state, {payload}) => {
+        const currentList = state[propName] || []
+
+        currentList.push(payload)
+
+        return ({...state, [propName]: currentList})
+    }
+})
+
+const listRemoveAction = (propName) => ({
+    [propNameToRemove(propName)]: (state, {payload}) => {
+        if (typeof state[propName] === 'undefined' || !state[propName].length) {
+            return state
+        }
+
+        return ({...state, [propName]: state[propName].filter((item) => item !== payload)})
+    }
+})
+
+const propToHandler = (propName) => {
+    if (isListProp(propName)) {
+        return ({
+            ...setterAction(propName),
+            ...listAddAction(propName),
+            ...listRemoveAction(propName),
+        })
+    } else {
+        return ({
+            ...setterAction(propName)
+        })
+    }
+}
+
 const reduceToHandler = (props) => props.reduce(
-    (handlers, name) => ({
+    (handlers, propName) => ({
         ...handlers,
-        [name]: (state, {payload}) => ({...state, [name]: payload})
+        ...propToHandler(propName)
     }), {})
 
 const makeReducerForProps = (props) => {
@@ -14,26 +57,44 @@ const makeReducerForProps = (props) => {
     return handleActions(actionHandlers, {})
 }
 
+const propToAction = (propName) => {
+    if (isListProp(propName)) {
+        return ({
+            [propName]: createAction(propName),
+            [propNameToAdd(propName)]: createAction(propNameToAdd(propName)),
+            [propNameToRemove(propName)]: createAction(propNameToRemove(propName)),
+        })
+    } else {
+        return ({
+            [propName]: createAction(propName)
+        })
+    }
+}
+
 const reduceToActions = (props) => props.reduce(
-    (actions, name) => ({
+    (actions, propName) => ({
         ...actions,
-        [name]: createAction(name)
+        ...propToAction(propName)
     }), {})
 
 const makeActionsForProps = (props) => props.length
     ? reduceToActions(props)
     : {}
 
-const makeDispatchToProps = (props, actions) => (dispatch) => props.reduce(
-    (map, prop) => ({
-        ...map,
-        [prop]: (value) => dispatch(actions[prop](value))
-    }), {})
+const makeDispatchToProps = (actions) => (dispatch) => {
+    let result = {}
+
+    for (const actionName in actions) {
+        result[actionName] = (value) => dispatch(actions[actionName](value))
+    }
+
+    return result
+}
 
 export default (stateProps) => {
     const reducer = makeReducerForProps(stateProps)
     const actions = makeActionsForProps(stateProps)
-    const getDispatchToProps = makeDispatchToProps(stateProps, actions)
+    const getDispatchToProps = makeDispatchToProps(actions)
 
     return {
         reducer,
--- dictator.spec.js
+++ dictator.spec.js
@@ -45,6 +45,67 @@ describe('Redux Dictator', () => {
                 c: 1
             })
         })
+
+        describe('List', () => {
+            it('adds an item to an empty list', () => {
+                const {reducer} = dictator(['itemList'])
+
+                expect(reducer({}, {
+                    type: 'itemListAdd',
+                    payload: 9
+                })).toEqual({
+                    itemList: [9]
+                })
+            })
+
+            it('adds an item to the list', () => {
+                const {reducer} = dictator(['urlList'])
+
+                expect(reducer({
+                    urlList: [1]
+                }, {
+                    type: 'urlListAdd',
+                    payload: 2
+                })).toEqual({
+                    urlList: [1, 2]
+                })
+            })
+
+            it('removing an item from an empty list returns an empty list', () => {
+                const {reducer} = dictator(['numberList'])
+
+                expect(reducer({
+                    numberList: []
+                }, {
+                    type: 'numberListRemove',
+                    payload: 1,
+                })).toEqual({
+                    numberList: []
+                })
+            })
+
+            it('removing an item from an undefined list leaves the list undefined', () => {
+                const {reducer} = dictator(['numberList'])
+
+                expect(reducer({}, {
+                    type: 'numberListRemove',
+                    payload: 1,
+                })).toEqual({})
+            })
+
+            it('removes an item from a list', () => {
+                const {reducer} = dictator(['urlList'])
+
+                expect(reducer({
+                    urlList: [1]
+                }, {
+                    type: 'urlListRemove',
+                    payload: 1
+                })).toEqual({
+                    urlList: []
+                })
+            })
+        })
     })
 
     describe('Actions', () => {
@@ -68,11 +129,38 @@ describe('Redux Dictator', () => {
                 b: 2,
             })
         })
+
+        describe('List', () => {
+            it('creates an action to set the list', () => {
+                const {actions, reducer} = dictator(['urlList'])
+
+                expect(reducer({}, actions.urlList([1, 2]))).toEqual({
+                    urlList: [1, 2]
+                })
+            })
+
+            it('creates an action to add an item to the list', () => {
+                const {actions, reducer} = dictator(['urlList'])
+
+                expect(reducer({urlList: [1]}, actions.urlListAdd(2))).toEqual({
+                    urlList: [1, 2]
+                })
+            })
+
+            it('creates an action to remove an item to the list', () => {
+                const {actions, reducer} = dictator(['urlList'])
+
+                expect(reducer({urlList: [1]}, actions.urlListRemove(1))).toEqual({
+                    urlList: []
+                })
+            })
+        })
     })
 
     describe('Map Dispatch to Props', () => {
-        it('returns a dispatch mapping for all actions', () => {
-            const dispatch = jest.fn()
+        const dispatch = jest.fn()
+
+        it('returns a dispatch mapping for all setters', () => {
             const stateProps = ['price', 'amount']
             const {getDispatchToProps} = dictator(stateProps)
             const mapDispatchToProps = getDispatchToProps(dispatch)
@@ -82,6 +170,26 @@ describe('Redux Dictator', () => {
 
             mapDispatchToProps.amount(2)
             expect(dispatch).toHaveBeenCalledWith({type: 'amount', payload: 2})
+        })
+
+        describe('List', () => {
+            it('returns a dispatch mapping for list add', () => {
+                const stateProps = ['consoleList']
+                const {getDispatchToProps} = dictator(stateProps)
+                const mapDispatchToProps = getDispatchToProps(dispatch)
+
+                mapDispatchToProps.consoleListAdd('PS4')
+                expect(dispatch).toHaveBeenCalledWith({type: 'consoleListAdd', payload: 'PS4'})
+            })
+
+            it('returns a dispatch mapping for list remove', () => {
+                const stateProps = ['consoleList']
+                const {getDispatchToProps} = dictator(stateProps)
+                const mapDispatchToProps = getDispatchToProps(dispatch)
+
+                mapDispatchToProps.consoleListRemove('XBOX')
+                expect(dispatch).toHaveBeenCalledWith({type: 'consoleListRemove', payload: 'XBOX'})
+            })
         })
     })
 })
