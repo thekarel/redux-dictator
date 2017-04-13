97f7da1f958d95ac0c35443ad2c3587ea2e7867d
Charles Szilagyi <charles@charlesagile.com>
2017-04-13 23:16:11 +0100

    Transpile ES6 arrow functions

--- dist/dictator.js
+++ dist/dictator.js
@@ -2,44 +2,60 @@
 
 var _reduxActions = require('redux-actions');
 
-const isListProp = propName => propName.endsWith('List');
+const isListProp = function (propName) {
+    return propName.endsWith('List');
+};
 
-const propNameToSet = propName => `${propName}Set`;
-const propNameToAdd = propName => `${propName}Add`;
-const propNameToRemove = propName => `${propName}Remove`;
+const propNameToSet = function (propName) {
+    return `${propName}Set`;
+};
+const propNameToAdd = function (propName) {
+    return `${propName}Add`;
+};
+const propNameToRemove = function (propName) {
+    return `${propName}Remove`;
+};
 
-const setterAction = propName => ({
-    [propNameToSet(propName)]: (state, _ref) => {
-        let payload = _ref.payload;
-        return Object.assign({}, state, { [propName]: payload });
-    }
-});
-
-const listAddAction = propName => ({
-    [propNameToAdd(propName)]: (state, _ref2) => {
-        let payload = _ref2.payload;
-
-        const currentList = state[propName] || [];
-
-        currentList.push(payload);
-
-        return Object.assign({}, state, { [propName]: currentList });
-    }
-});
-
-const listRemoveAction = propName => ({
-    [propNameToRemove(propName)]: (state, _ref3) => {
-        let payload = _ref3.payload;
-
-        if (typeof state[propName] === 'undefined' || !state[propName].length) {
-            return state;
+const setterAction = function (propName) {
+    return {
+        [propNameToSet(propName)]: function (state, _ref) {
+            let payload = _ref.payload;
+            return Object.assign({}, state, { [propName]: payload });
         }
+    };
+};
 
-        return Object.assign({}, state, { [propName]: state[propName].filter(item => item !== payload) });
-    }
-});
+const listAddAction = function (propName) {
+    return {
+        [propNameToAdd(propName)]: function (state, _ref2) {
+            let payload = _ref2.payload;
 
-const propToHandler = propName => {
+            const currentList = state[propName] || [];
+
+            currentList.push(payload);
+
+            return Object.assign({}, state, { [propName]: currentList });
+        }
+    };
+};
+
+const listRemoveAction = function (propName) {
+    return {
+        [propNameToRemove(propName)]: function (state, _ref3) {
+            let payload = _ref3.payload;
+
+            if (typeof state[propName] === 'undefined' || !state[propName].length) {
+                return state;
+            }
+
+            return Object.assign({}, state, { [propName]: state[propName].filter(function (item) {
+                    return item !== payload;
+                }) });
+        }
+    };
+};
+
+const propToHandler = function (propName) {
     if (isListProp(propName)) {
         return Object.assign({}, setterAction(propName), listAddAction(propName), listRemoveAction(propName));
     } else {
@@ -47,15 +63,19 @@ const propToHandler = propName => {
     }
 };
 
-const reduceToHandler = props => props.reduce((handlers, propName) => Object.assign({}, handlers, propToHandler(propName)), {});
+const reduceToHandler = function (props) {
+    return props.reduce(function (handlers, propName) {
+        return Object.assign({}, handlers, propToHandler(propName));
+    }, {});
+};
 
-const makeReducerForProps = props => {
+const makeReducerForProps = function (props) {
     const actionHandlers = props.length ? reduceToHandler(props) : {};
 
     return (0, _reduxActions.handleActions)(actionHandlers, {});
 };
 
-const propToAction = propName => {
+const propToAction = function (propName) {
     if (isListProp(propName)) {
         return {
             [propNameToSet(propName)]: (0, _reduxActions.createAction)(propNameToSet(propName)),
@@ -69,21 +89,31 @@ const propToAction = propName => {
     }
 };
 
-const reduceToActions = props => props.reduce((actions, propName) => Object.assign({}, actions, propToAction(propName)), {});
-
-const makeActionsForProps = props => props.length ? reduceToActions(props) : {};
-
-const makeDispatchToProps = actions => dispatch => {
-    let result = {};
-
-    for (const actionName in actions) {
-        result[actionName] = value => dispatch(actions[actionName](value));
-    }
-
-    return result;
+const reduceToActions = function (props) {
+    return props.reduce(function (actions, propName) {
+        return Object.assign({}, actions, propToAction(propName));
+    }, {});
 };
 
-module.exports = stateProps => {
+const makeActionsForProps = function (props) {
+    return props.length ? reduceToActions(props) : {};
+};
+
+const makeDispatchToProps = function (actions) {
+    return function (dispatch) {
+        let result = {};
+
+        for (const actionName in actions) {
+            result[actionName] = function (value) {
+                return dispatch(actions[actionName](value));
+            };
+        }
+
+        return result;
+    };
+};
+
+module.exports = function (stateProps) {
     const reducer = makeReducerForProps(stateProps);
     const actions = makeActionsForProps(stateProps);
     const getDispatchToProps = makeDispatchToProps(actions);
